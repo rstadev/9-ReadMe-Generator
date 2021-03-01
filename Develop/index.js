7faf65bc2302f99eae7f3eca13df8f6dc9e38925
unknown <nukasine@gmail.com>
2021-03-01 17:39:40 -0500

    setup inquirer and inquirer prompt. Have major quesitons in questions array.

--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,47 @@
 // TODO: Include packages needed for this application
-var inquirer = require("inquirer");
-var fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const md = require("./utils/generateMarkdown")
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "user",
+    message: "What is your github username?"
+  },
+  {
+    type:"input",
+    name:"title",
+    message:"What is the title of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "How would you describe this project?",
+  },
+  {
+    type:"input",
+    name:"usage",
+    message:"Enter in usage informaton.",
+  },
+//   {
+//     type:"",
+//     name:"",
+//     message:"",
+//   },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  const responses = inquirer.prompt(questions);
+
+  console.log(responses)
+}
 
 // Function call to initialize app
 init();
